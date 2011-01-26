7c24636b98215bb57b1d914ca85431f835f11fb7
preek <alain.lafon@dispatched.ch>
2011-01-26 12:51:26 +0100

    Configure for production

--- pomodoro_timer.sh
+++ pomodoro_timer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 LOGFILE=pomodoro_backlog.asc
-POMODORO_TIME=5
-BREAK_TIME=1
+POMODORO_TIME=25
+BREAK_TIME=5
 
 clear
 echo "What is your activity for this pomodoro? "
@@ -14,7 +14,7 @@ function timer {
   counter=$limit
   echo = $2 =; echo
   echo "$counter minutes left...";
-  sleep 1
+  sleep 60
   let "counter = $counter - 1"
 
   while [ $counter != 0 ]; do
